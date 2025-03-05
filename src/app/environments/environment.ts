f2022e57e9be12dac0c0234d19f1a9fd34912aae
NguyenThien02 <ngpthien02@gmail.com>
2025-03-05 21:49:53 +0700

    Update deploy v4

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
     apiBaseUrl: 'https://clinic-g9cc.onrender.com/api/clinic',
+    // apiBaseUrl: 'http://localhost:8080/api/clinic',
 };
